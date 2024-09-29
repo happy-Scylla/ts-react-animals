e9457026470b3a6595606b273ee8bc0ff9a8533d
happy-Scylla <tim.weise81@gmail.com>
2024-09-30 00:16:21 +0200

    fixed storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,5 +14,16 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
+  webpackFinal: async (config) => {
+    config.module?.rules?.push({
+      test: /\.(png|jpg|jpeg|gif|svg)$/i,
+      type: 'asset/resource',  // This is built-in Webpack 5 support for handling images
+      generator: {
+        filename: 'images/[name].[ext]',  // Control output directory and name format
+      },
+    });
+
+    return config;
+  },
 };
 export default config;
